1dd1df6a3e0c1c5a186ddcae5620648df0698850
Collins Chima-Anyanka <77591582+nnam-nnam@users.noreply.github.com>
2026-02-15 17:28:57 -0500

    Create Environment sturct for binding identifiers to variables

--- src/monkey/repl/repl.go
+++ src/monkey/repl/repl.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"monkey/evaluator"
 	"monkey/lexer"
+	"monkey/object"
 	"monkey/parser"
 )
 
@@ -18,6 +19,7 @@ From there the output is evaluated.
 */
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
 
 	for {
 		fmt.Fprint(out, PROMPT)
@@ -36,7 +38,7 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		evaluated := evaluator.Eval(program)
+		evaluated := evaluator.Eval(program, env)
 		if evaluated != nil {
 			io.WriteString(out, evaluated.Inspect())
 			io.WriteString(out, "\n")
